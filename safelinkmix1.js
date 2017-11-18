43f7b98dcbc0efa9ecf8b71ab54aee262abd355c
Lafuan13 <lafuan13@gmail.com>
2017-11-19 02:06:33 +0700

    Update safelinkmix1.js

--- safelinkmix1.js
+++ safelinkmix1.js
@@ -19,13 +19,14 @@ var a_to_ve="";var a_to_vf=new Array();var a_to_vg=0;a_to_ve=document.getElement
 j++;}
 if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
-var x=Math.floor((Math.random()*5)+ 1);
+var x=Math.floor((Math.random()*6)+ 1);
 var xxx=null;
 if(x=="1"){xxx="/2017/11/koenigsegg-ccxr-trevita-48-million.html"}
 if(x=="2"){xxx="/2017/11/lamborghini-veneno-45-million.html"}
 if(x=="3"){xxx="/2017/11/w-motors-lykan-hypersport-34-million.html"}
 if(x=="4"){xxx="/2017/11/bugatti-veyron-limited-edition-mansory-vivere-34-million.html"}
-if(x=="5"){xxx="/2017/11/ferrari-pininfarina-sergio-3-million.html"}a_to_ve[i].href="http://www.ensiklopedi.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2017/11/ferrari-pininfarina-sergio-3-million.html"}
+if(x=="6"){xxx="/2017/11/tire-maintenance-basic-guide.html"}a_to_ve[i].href="http://www.ensiklopedi.ga"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
